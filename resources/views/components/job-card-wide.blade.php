9e290c7871cad3bebf295adf196de399a6dcff06
aboodabdallah37@gmail.com <88708480+Abdalrahman-Abdallah-Ahmed@users.noreply.github.com>
2025-10-25 01:36:47 +0300

    frontend stuff

---
+++ resources/views/components/job-card-wide.blade.php
@@ -0,0 +1,15 @@
+<x-panel class="flex gap-x-6 flex-row">
+    <div>
+        <x-employer-logo />
+    </div>
+    <div class="flex-1 flex flex-col">
+        <a href="#" class="self-start text-sm text-gray-400">Laracasts</a>
+        <h3 class="font-bold mt-3 group-hover:text-blue-800 text-xl transition-colors duration-1000">Video Editor</h3>
+        <p class="text-gray-400 text-sm mt-auto">Full Time - From $60.000</p>
+    </div>
+    <div>
+        <x-tag>Tag</x-tag>
+        <x-tag>Tag</x-tag>
+        <x-tag>Tag</x-tag>
+    </div>
+</x-panel>
